eb71b6f316fb8db049ad114ab440e6a24277290f
davidbabaev <davidbabaev198@gmail.com>
2026-05-30 22:16:03 +0300

    created new folder with reserved Nextjs route.ts file, page.tsx testing and error.tsx file test

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,12 +1,33 @@
 import Counter from './Counter';
+export const dynamic = 'force-dynamic';
+
+async function getMessages(){
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  
+  if(Math.random() < 0.3){
+    throw new Error("Failed to fetch messages");
+  }
+
+  return [
+    {id: 1, text: 'First message'},
+    {id: 2, text: 'Second message'},
+  ];
+}
 
 export default async function PlaygroundPage() {
+  // throw new Error("Boom - forcing the error boudary to show")
+  // await new Promise((resolve) => setTimeout(resolve, 2000))
 
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const message = await getMessages();
 
   return (
     <div style={{padding: 40}}>
         <h1>Playground</h1>
+        <ul>
+          {message.map((m) => (
+            <li key={m.id}>{m.text}</li>
+          ))}
+        </ul>
         <Counter/>
     </div>
   )
