e0c3669ba4d1bb6ce859bf391ea00efbbd745d1c
davidbabaev <davidbabaev198@gmail.com>
2026-05-31 23:06:21 +0300

    POST operation added to route.ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,3 +3,8 @@ export async function GET(request: Request){
     const name = searchParams.get('name') ?? 'stranger'; // pull out ?name=...
     return Response.json({message: `Hello ${name}`});
 }
+
+export async function POST(request: Request){
+    const body = await request.json();
+    return Response.json({youSent: body});
+}
