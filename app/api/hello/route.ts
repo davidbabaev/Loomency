2be0dcddf567f0c95c406035afbe6e1a4fb302b8
davidbabaev <davidbabaev198@gmail.com>
2026-05-31 20:54:11 +0300

    created new GET operation with Next syntax

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,5 @@
-
-export async function GET(){
-    return Response.json({
-        //
-    })
+export async function GET(request: Request){
+    const {searchParams} = new URL(request.url); // parse the url
+    const name = searchParams.get('name') ?? 'stranger'; // pull out ?name=...
+    return Response.json({message: `Hello ${name}`});
 }
