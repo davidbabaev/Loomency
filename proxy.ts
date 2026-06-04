cc6146ddc2fdc5cd5d3c969cec112804f4f436e8
davidbabaev <davidbabaev198@gmail.com>
2026-06-04 22:55:17 +0300

    proxy.ts added - middleware

---
+++ proxy.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function proxy(request: NextRequest){
+    console.log('Proxy ran for:', request.nextUrl.pathname);
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/((?!_next/static|_next/image|favicon.ico).*)']
+}
