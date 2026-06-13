10e7e44985b93d4e97947cc8a30cf17a60bbc238
davidbabaev <davidbabaev198@gmail.com>
2026-06-13 20:19:42 +0300

    feat: redirect unuathenticated users from protected routes in proxy

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,15 @@
+import { getSessionCookie } from "better-auth/cookies";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default function proxy(request: NextRequest){
-    console.log('Proxy ran for:', request.nextUrl.pathname);
+
+    const sessionCookie = getSessionCookie(request);
+
+    if(!sessionCookie && request.nextUrl.pathname.startsWith('/dashboard')){
+        console.log('Proxy ran for:', request.nextUrl.pathname);
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
     return NextResponse.next();
 }
 
