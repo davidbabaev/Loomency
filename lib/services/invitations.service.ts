41040bc2ed0a8da43d7bafb42f82bd2cb109325d
davidbabaev <davidbabaev198@gmail.com>
2026-09-22 23:29:50 +0300

    ConflictError added into errors.ts, worked on invitations.service file

--- lib/services/invitations.service.ts
+++ lib/services/invitations.service.ts
@@ -1,6 +1,32 @@
 import z from "zod";
+import { CreateEmployeeSchema } from "../validations/employees.schema";
+import { getEmployeeByUserId } from "../repositories/employees.repository";
+import { ConflictError, ForbiddenError } from "../errors";
+import { getPendingInvitationEmail, insertInvitation } from "../repositories/invitations.repository";
+import { generateToken, hashToken } from "../tokens";
 
 export async function createInvitation(
     userId: string,
-    data: z.infer<typeof CreateEm>
-){}
+    data: z.infer<typeof CreateEmployeeSchema>
+){
+    const employee = await getEmployeeByUserId(userId);
+    if(!employee){
+        throw new ForbiddenError('Access denied')
+    }
+    
+    if(employee.role !== 'admin'){
+        throw new ForbiddenError('Admin role required')
+    }
+
+    const existing = await getPendingInvitationEmail(data.email, employee.business_id)
+    if(existing){
+        throw new ConflictError('An invitation is already pending for this email');
+    }
+
+    const token =  generateToken();
+    const hashedToken = hashToken(token);
+
+    const expired = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+    // const insert = await insertInvitation()
+}
